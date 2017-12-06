99c5706a5a003d6d8b00efd4ca5d1428abbe2b59
Colin Richardson <richardsonjcolin@gmail.com>
2017-12-06 00:26:50 +0000

    display total revenue, average revenue of all stores. Display stores generating more than 1,000,000 in annual sales

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,7 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_store_revenue = Store.sum(:annual_revenue)
+puts "Revenue of all stores: $#{@total_store_revenue}"
+puts "Average revenue of all stores: $#{@total_store_revenue / Store.count}"
+puts "Number of stores generating more than $1,000,000 in annual sales: #{Store.where("annual_revenue > ?", 1000000).count}"
