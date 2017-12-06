bc55efe8fddcba3f8b93dd4920bf84e7e7ef507a
Colin Richardson <richardsonjcolin@gmail.com>
2017-12-06 00:37:20 +0000

    create 6 new employess to the employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Randy", last_name: "Dandy", hourly_rate: 99)
+@store1.employees.create(first_name: "Pauli", last_name: "Parm", hourly_rate: 10)
+@store1.employees.create(first_name: "Fran", last_name: "Siss", hourly_rate: 15)
+@store2.employees.create(first_name: "Marv", last_name: "Madness", hourly_rate: 175)
+@store2.employees.create(first_name: "Bethany", last_name: "Brave", hourly_rate: 100)
+@store2.employees.create(first_name: "Mr", last_name: "Roboto", hourly_rate: 1)
